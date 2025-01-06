188536fae88cbb57ed9607cfa06b58a3c9ace4e0
jps27CSE <jackpritomsoren@hotmail.com>
2025-01-06 14:47:18 +0600

    added changes in no check of eslint and ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript type checking during build
+  },
 };
 
 export default nextConfig;
